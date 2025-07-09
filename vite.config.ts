06756a5979a83ef7cf7a79b29e1fc183be9d1b21
Gian <gianclaudio.gc@gmail.com>
2025-07-09 20:27:11 +0200

    changing path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for relative paths in Azure
+  base: './', // important for relative paths in Azure
 });
